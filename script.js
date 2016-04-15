62cc80c88e3af3a3e4379b399fa2d7e550d4f474
Roderic Day <roderic.day@gmail.com>
2016-04-15 16:21:57 -0400

    Reports

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-range = n => "_".repeat(n+1).split('').map((c, i) => [String.fromCharCode(65+i), i+1])
+// javascript is hilarious
+range = n => "_".repeat(n+1).split('').map((c, i) => [String.fromCharCode(65+i), i+1]);
+roundTo = (n, d) => Math.round(n * Math.pow(10, d)) * Math.pow(10, -d);
 
 function changeSize(dnrow, dncol, cell) {
     data.nrow = Math.max(0, data.nrow+dnrow);
@@ -32,7 +34,7 @@ function render() {
 }
 
 function generateTable(data) {
-    return React.createElement("table", {key: "table"},
+    return React.createElement("table", {id: "table", key: "table"},
         React.createElement("tbody", {},
         range(data.nrow).map(([_,r]) => React.createElement("tr", {key: r},
         range(data.ncol).map(([c,_]) => React.createElement("td", {key: c},
@@ -44,10 +46,11 @@ function generateTable(data) {
 
 function generateButtons() {
     return React.createElement("div", {key: "buttonBar"}, [
-        ["C-", _ => changeSize( 0,-1)],
-        ["C+", _ => changeSize( 0, 1)],
         ["R-", _ => changeSize(-1, 0)],
         ["R+", _ => changeSize( 1, 0)],
+        ["C-", _ => changeSize( 0,-1)],
+        ["C+", _ => changeSize( 0, 1)],
+        ["report", report],
         ["clear", clear],
         ["reset", reset],
     ].map(([label, action], i) =>
@@ -55,7 +58,48 @@ function generateButtons() {
 }
 
 function generateRoot(data) {
-  return React.createElement("div", {}, [generateButtons(), generateTable(data)]);
+    return React.createElement("div", {}, [generateButtons(), generateTable(data)]);
+}
+
+function report() {
+    // operate on visual data... just... cause...
+    var names = Array.from(table.rows[0].cells).slice(3).map(cell => cell.firstChild.value);
+    // set balances to zero for all participants
+    var netBalances = {};
+    names.forEach(name => netBalances[name] = 0);
+    // extract the data that matters from the table
+    var transactions = Array.from(table.rows)
+        .slice(1) // skip first row
+        .map(row => Array.from(row.cells)
+            .map(cell => cell.firstChild.value) // get data
+            .map((value, i) => i > 1 && value ? parseFloat(value) : value)
+            .slice(1)) // skip description
+        .filter(row => row[1]); // ignore transactions without debtor
+    // obtain net balances of debt
+    var tally = transactions.map(([person, amount, ...shares]) => shares.reduce((a, b)=>a+b));
+    transactions.map(([person, amount, ...shares], i) => netBalances[person] += amount);
+    transactions.map(([person, amount, ...shares], i) => shares.map((share, j) =>
+        netBalances[names[j]] -= amount*share/tally[i]));
+    // figure out payments required by clashing most indebted vs. most owed until resolution
+    // we should at most need N-1 clashes, otherwise something went wrong
+    var payments = [], N = names.length;
+    for (i=0; i<N; i++) {
+        names = names.filter(name => Math.abs(netBalances[name]) > 0.1)
+                     .sort((a, b) => netBalances[a] > netBalances[b]);
+        // success condition
+        if (names.length==0) {
+            alert(payments.join('\n'));
+            return
+        }
+        var [A, B] = [names[0], names[names.length-1]];
+        var exchange = Math.min(-netBalances[A], netBalances[B]);
+        netBalances[A] += exchange;
+        netBalances[B] -= exchange;
+        payments.push(`${A} owes ${B} $${roundTo(exchange, 2)}`);
+    }
+    // error condition
+    alert("There was an error.");
+    throw JSON.stringify(netBalances);
 }
 
 data = null;
